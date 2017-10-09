d8a413b0731e688a07751c7640df76ab9ac93e63
pkarira <pulkit5208@gmail.com>
2017-10-10 00:00:53 +0530

    minor changes

--- index.js
+++ index.js
@@ -2,15 +2,25 @@ var express=require('express');
 var app=express();
 var socket = require('socket.io');
 var server =app.listen(8080,'10.42.0.1');
+// var Player = require('player');
+//var player = new Player('/home/pulkit/Downloads/music.mp3');
+//var load = require('audio-loader')
+// player.play(function(err, player){
+// console.log('playend!');
+// });
+// load('/home/pulkit/Downloads/music.mp3').then(function (buffer) {
+//   play(buffer);
+// });
 app.set('view engine','ejs');
 app.use(express.static('public'));
 var io = socket(server);
 io.on('connection',function(socket) {
     console.log('made socket connection', socket.id);
     socket.on('reject', function(data){
-       io.sockets.emit('reject', data);
+       socket.broadcast.emit('reject', data);
    });
    socket.on('contactInfo', function(data){
+       console.log("receiver contact");
        socket.broadcast.emit('contactInfo', data);
    });
 });
--- public/client.js
+++ public/client.js
@@ -1,21 +1,24 @@
 var socket = io.connect('http://10.42.0.1:8080');
+// var Player = require('player');
+// var player = new Player('/home/pulkit/Downloads/music.mp3');
 var number = document.getElementById('number'),
       btn = document.getElementById('reject'),
       name = document.getElementById('name'),
       test=document.getElementById('test');
 btn.addEventListener('click', function(){
-  console.log("csjhsjd");
   socket.emit('reject', {
-      message: "reject",
-  });
-  message.value = "";
-});
-test.addEventListener('click', function(){
-  socket.emit('contactInfo', {
-      number: "isdfdsf",
-      name:"cdddfdff",
+      message: "reject"
   });
 });
+// test.addEventListener('click', function(){
+// //   player.play(function(err, player){
+// //   console.log('playend!');
+// // });
+//   socket.emit('contactInfo', {
+//       number: "isdfdsf",
+//       name:"cdddfdff",
+//   });
+// });
 socket.on('contactInfo', function(data){
     name.innerHTML = data.name  ;
     number.innerHTML =  data.number;
