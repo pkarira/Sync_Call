b9667ff4a2c77672bd8babfe98f69150ed95fa74
pkarira <pulkit5208@gmail.com>
2017-10-11 18:09:10 +0530

    minor changes

--- index.js
+++ index.js
@@ -2,15 +2,6 @@ var express=require('express');
 var app=express();
 var socket = require('socket.io');
 var server =app.listen(8080,'10.42.0.1');
-// var Player = require('player');
-//var player = new Player('/home/pulkit/Downloads/music.mp3');
-//var load = require('audio-loader')
-// player.play(function(err, player){
-// console.log('playend!');
-// });
-// load('/home/pulkit/Downloads/music.mp3').then(function (buffer) {
-//   play(buffer);
-// });
 app.set('view engine','ejs');
 app.use(express.static('public'));
 var io = socket(server);
@@ -21,6 +12,11 @@ io.on('connection',function(socket) {
    });
    socket.on('contactInfo', function(data){
        console.log("receiver contact");
+       console.log(data.name);
+       console.log(data.number);
        socket.broadcast.emit('contactInfo', data);
+   });
+   socket.on('pick', function(data){
+       socket.broadcast.emit('pick', data);
    });
 });
--- public/client.js
+++ public/client.js
@@ -1,25 +1,25 @@
 var socket = io.connect('http://10.42.0.1:8080');
 // var Player = require('player');
-// var player = new Player('/home/pulkit/Downloads/music.mp3');
 var number = document.getElementById('number'),
       btn = document.getElementById('reject'),
       name = document.getElementById('name'),
-      test=document.getElementById('test');
+      pick=document.getElementById('pick');
+// var player = new Player('/home/pulkit/Downloads/music.mp3');
 btn.addEventListener('click', function(){
+  // player.stop()
   socket.emit('reject', {
       message: "reject"
   });
 });
-// test.addEventListener('click', function(){
-// //   player.play(function(err, player){
-// //   console.log('playend!');
-// // });
-//   socket.emit('contactInfo', {
-//       number: "isdfdsf",
-//       name:"cdddfdff",
-//   });
-// });
+pick.addEventListener('click',function(){
+  // player.stop()
+  socket.emit('pick', {
+      message: "pick"
+  });
+});
 socket.on('contactInfo', function(data){
-    name.innerHTML = data.name  ;
-    number.innerHTML =  data.number;
+    // player.play(function(err, player){
+    //  });
+    document.getElementById('name').innerHTML+= data.name;
+    document.getElementById('number').innerHTML+=  data.number;
 });
